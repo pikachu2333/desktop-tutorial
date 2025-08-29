e0e9e55545910698705af038c9400635c5d4f685
pikachu2333 <yzy3059702271@gmail.com>
2025-08-29 22:54:02 +0800

    pikachu
    init

---
+++ backups/restore-script.bat
@@ -0,0 +1,5 @@
+@echo off 
+C:
+cd "C:\service\fabric1.20.1\mods"
+java -jar "[高级备份] AdvancedBackups-fabric-1.20-3.7.1.jar"
+pause
---
+++ run.bat
@@ -0,0 +1,1 @@
+java -Xmx8G -jar fabric-server-mc.1.20.1-loader.0.17.2-launcher.1.1.0.jar nogui
